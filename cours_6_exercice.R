3332568b84c414de47c0bf5316630b7b50b7751d
Juliette Leblanc <juliette.leblanc99@gmail.com>
2022-10-18 18:17:23 -0400

    tâche 1

---
+++ cours_6_exercice.R
@@ -0,0 +1,12 @@
+#gitcreds::gitcreds_set() #mettre notre Token
+
+#### Taches 1 - recoller les morceaux ####
+df_1 <- read.csv("data/data_1.csv")
+df_2 <- read.csv("data/data_2.csv")
+df_3 <- read.csv("data/data_3.csv")
+df_4 <- read.csv("data/data_4.csv")
+
+#list.files(path = ".", pattern = "csv") #montre les ficheirs csv qu'on a 
+
+df_rbind <- rbind(df_1, df_2, df_3, df_4) # combiner 4 objets dans 1
+
